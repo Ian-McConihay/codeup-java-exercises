923f53b984b5ead904e658f5d48af81fccf43b03
Ian McConihay <mcconihay.ian92@gmail.com>
2021-10-21 16:19:13 -0500

    Java Baby

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,37 @@
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+        System.out.println(" I am Here!");
+        int myFavortiteNumber = 18;
+        System.out.println(myFavortiteNumber);
+        String myString = "All Might";
+        System.out.println(myString);
+//        long myNumber;
+//        java: variable myNumber might not have been initialized
+        long myNumber = 123l;
+        myNumber = 123;
+        System.out.println(myNumber);
+        float myNewNumber = 3.14f;
+        System.out.println(myNewNumber);
+
+
+        int y = 5;
+        System.out.println(y++);
+        System.out.println(y);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        class
+
+
+
+
+
+
+
+
+
+    }
 }

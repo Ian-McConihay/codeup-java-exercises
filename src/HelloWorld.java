cf4353c7a6183d3ffbbd2db55dad737f4cfa8e6f
Ian McConihay <mcconihay.ian92@gmail.com>
2021-10-22 09:13:06 -0500

    Java Baby

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -14,22 +14,34 @@ public class HelloWorld {
         float myNewNumber = 3.14f;
         System.out.println(myNewNumber);
 
+//
+//        int y = 5;
+//        System.out.println(y++);
+//        System.out.println(y);
 
-        int y = 5;
-        System.out.println(y++);
-        System.out.println(y);
-
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
 
 //        class
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
 
+//        int three = (int) "three";
 
-
-
-
-
+//
+//        int x = 4;
+//        x += 5;
+//
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x
 
 
 

5d7b4aa69b128f1d88c193242a9bfae5a68aa059
Ian McConihay <mcconihay.ian92@gmail.com>
2021-10-25 14:06:44 -0500

    Loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,42 +1,72 @@
+import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-    System.out.println("I am Here!");
+        System.out.println("I am Here!");
 //    do not use == signs with stings in java
-        boolean isSunny = false;
-        boolean hasUmbrella = false;
-        if(isSunny){
-            System.out.println("Cool man you are ready! Grab the Umbrella");
-        }
-        else if (!isSunny && hasUmbrella){
-            System.out.println("Cool" );
-        }
-        else {
-            System.out.println("Go check the weather");
-        }
+//        boolean isSunny = false;
+//        boolean hasUmbrella = false;
+//        if(isSunny){
+//            System.out.println("Cool man you are ready! Grab the Umbrella");
+//        }
+//        else if (!isSunny && hasUmbrella){
+//            System.out.println("Cool" );
+//        }
+//        else {
+//            System.out.println("Go check the weather");
+//        }
+//
+//        int bee = 1;
+//        switch(bee){
+//
+//        }
+//
+//        char userInput = 'n';
+//        do{
+//            System.out.println("du hast");
+//        } while (userInput == 'y');
+//    while(userInput == 'y')
 
-        int bee = 1;
-        switch(bee){
-
-        }
-
-        char userInput = 'n';
 
 
-
-                int i = 5;
-        while(i <= 15) {
-            System.out.println("i is " + i);
-            i++;
-        }
+//                int i = 5;
+//        while(i <= 15) {
+//            System.out.println("i is " + i);
+//            i++;
+//        }
 
 //        int i = 0;
 //        do {
 //            System.out.println(i * 2);
 //            i++;
 //        } while(i <= 100);
+//
+//        for (int i = 1; i < 50; i++) {
+//            if (i % 15 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0){
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else  System.out.println(i);
+//        }
+    Scanner scanner = new Scanner(System.in);
 
 
+        System.out.println("Do you wanna play a game?");
+        boolean yes = false;
+        boolean no = false;
+        if(yes){
+            System.out.println("enter an integer");
+        }
+        else if (no){
+            System.out.println("Go check the weather");
+        }
+        String play = scanner.next();
+
+        System.out.printf("Y/N");
+
+        int A = 90 < 100;
 
 
+        }
     }
-}

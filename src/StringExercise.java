ab225a2465cf47edf8fe91a1315cabe940b201db
Ian McConihay <mcconihay.ian92@gmail.com>
2021-10-26 10:45:25 -0500

    Strings

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,41 @@
 public class StringExercise {
     public static void main(String[] args) {
-            System.out.print("Hello, World!");
-            }
-        }
+//            System.out.print("Hello, World!");
+            String message = "We don't need no education";
+            System.out.println(message);
+           String messageOne = message.replace("education", "thought control");
+            System.out.println(messageOne);
+            String messageTwo = messageOne.replace("We don't need no thought control", "Check \"This\" out, \"s inside of\"s! ");
+        System.out.println(messageTwo);
+        String messageThree = "In windows, the main drive is usually C:\\ ";
+        System.out.println(messageThree);
+        String messageFour = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(messageFour);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/StringsHandout.java
+++ src/StringsHandout.java
@@ -9,14 +9,14 @@ public class StringsHandout {
         //This leads us to use object comparison methods (.equals and family) to reliably check the VALUES of the objects vs. the REFERENCES of the objects
 
         //Don't do this! ! ! This is behavior from a woods-y topic related to how the Java Virtual Machine works
-        if("This is a string" == "This is a string"){
-            System.out.println("Performing some kind of super important operation with the above!! (Skynet launched!)");
-        }
+//        if("This is a string" == "This is a string"){
+//            System.out.println("Performing some kind of super important operation with the above!! (Skynet launched!)");
+//        }
 
         //Utilize the methods introduced here instead for consistency and reliability:
-        if("This is a string".equals("This is a string")){
-            System.out.println("Everything is a-okay over here! We used .equals, so we're feeling confident in what's going on with our code.");
-        }
+//        if("This is a string".equals("This is a string")){
+//            System.out.println("Everything is a-okay over here! We used .equals, so we're feeling confident in what's going on with our code.");
+//        }
 
         // ' ~ " * "string comparison methods" * " ~ '
 
@@ -33,51 +33,52 @@ public class StringsHandout {
         System.out.println("txCapitalCity.equalsIgnoreCase(lowercaseTxCapitalCity) = " + txCapitalCity.equalsIgnoreCase(lowercaseTxCapitalCity));
 
         //.startsWith + .endsWith
+        System.out.println();
         String austinSentence = "The capital city of Texas is " + txCapitalCity + ", and it is growing quickly.";
-        System.out.println(austinSentence);
-        System.out.println("austinSentence.startsWith(\"The capital city\") = " + austinSentence.startsWith("The capital city"));
-        System.out.println("austinSentence.startsWith(\"the capital city\") = " + austinSentence.startsWith("the capital city"));
-        System.out.println("austinSentence.startsWith(\"The capitol city\") = " + austinSentence.startsWith("The capitol city"));
-
-
-        System.out.println("austinSentence.endsWith(\"it is growing quickly.\") = " + austinSentence.endsWith("it is growing quickly."));
-        System.out.println("austinSentence.endsWith(\"it is growing QUICKLY.\") = " + austinSentence.endsWith("it is growing QUICKLY."));
-        System.out.println("austinSentence.endsWith(\"it is growing quick.\") = " + austinSentence.endsWith("it is growing quick."));
+//        System.out.println(austinSentence);
+//        System.out.println("austinSentence.startsWith(\"The capital city\") = " + austinSentence.startsWith("The capital city"));
+//        System.out.println("austinSentence.startsWith(\"the capital city\") = " + austinSentence.startsWith("the capital city"));
+//        System.out.println("austinSentence.startsWith(\"The capitol city\") = " + austinSentence.startsWith("The capitol city"));
+//        System.out.println();
+////
+//        System.out.println("austinSentence.endsWith(\"it is growing quickly.\") = " + austinSentence.endsWith("it is growing quickly."));
+//        System.out.println("austinSentence.endsWith(\"it is growing QUICKLY.\") = " + austinSentence.endsWith("it is growing QUICKLY."));
+//        System.out.println("austinSentence.endsWith(\"it is growing quick.\") = " + austinSentence.endsWith("it is growing quick."));
 
         //' ~ " * "string manipulation methods" * " ~ '
+//  -1 = not found
+//         .indexOf [search start to finish], .lastIndexOf [start end to finish], .charAt(int index)
+        System.out.println();
+//        System.out.println("austinSentence = " + austinSentence);
+//        System.out.println("austinSentence.indexOf('x') = " + austinSentence.indexOf('x'));
+//        System.out.println("austinSentence.charAt(20) = " + austinSentence.charAt(20));
+//        System.out.println("austinSentence.charAt(21) = " + austinSentence.charAt(21));
+//        System.out.println("austinSentence.charAt(22) = " + austinSentence.charAt(22));
+//        System.out.println("austinSentence.charAt(23) = " + austinSentence.charAt(23));
+//        System.out.println("austinSentence.charAt(24) = " + austinSentence.charAt(24));
+//        System.out.println();
+//        System.out.println("austinSentence.indexOf(\"capital city\") = " + austinSentence.indexOf("capital city"));
+//        System.out.println("austinSentence.indexOf(\"capitol city\") = " + austinSentence.indexOf("capitol city"));
+//        System.out.println();
+//        System.out.println("***************************************************************");
+//        System.out.println("** indexOf goes start to end, lastIndexOf goes end to start: **");
+//        System.out.println("***************************************************************");
+//        System.out.println(austinSentence);
+//        System.out.println("austinSentence.indexOf(\"is\") = " + austinSentence.indexOf("is"));
+//        System.out.println("austinSentence.lastIndexOf(\"is\") = " + austinSentence.lastIndexOf("is"));
+//        System.out.println();
+//
+//        //int length() - returns length of string
+//        System.out.println("austinSentence = " + austinSentence);
+//        System.out.println("austinSentence.length() = " + austinSentence.length());
+//        System.out.println("txCapitalCity = " + txCapitalCity);
+//        System.out.println("txCapitalCity.length() = " + txCapitalCity.length());
+//        System.out.println();
 
-        // .indexOf [search start to finish], .lastIndexOf [start end to finish], .charAt(int index)
-        System.out.println();
-        System.out.println("austinSentence = " + austinSentence);
-        System.out.println("austinSentence.indexOf('x') = " + austinSentence.indexOf('x'));
-        System.out.println("austinSentence.charAt(20) = " + austinSentence.charAt(20));
-        System.out.println("austinSentence.charAt(21) = " + austinSentence.charAt(21));
-        System.out.println("austinSentence.charAt(22) = " + austinSentence.charAt(22));
-        System.out.println("austinSentence.charAt(23) = " + austinSentence.charAt(23));
-        System.out.println("austinSentence.charAt(24) = " + austinSentence.charAt(24));
-        System.out.println();
-        System.out.println("austinSentence.indexOf(\"capital city\") = " + austinSentence.indexOf("capital city"));
-        System.out.println("austinSentence.indexOf(\"capitol city\") = " + austinSentence.indexOf("capitol city"));
-        System.out.println();
-        System.out.println("***************************************************************");
-        System.out.println("** indexOf goes start to end, lastIndexOf goes end to start: **");
-        System.out.println("***************************************************************");
-        System.out.println(austinSentence);
-        System.out.println("austinSentence.indexOf(\"is\") = " + austinSentence.indexOf("is"));
-        System.out.println("austinSentence.lastIndexOf(\"is\") = " + austinSentence.lastIndexOf("is"));
-        System.out.println();
-
-        //int length() - returns length of string
-        System.out.println("austinSentence = " + austinSentence);
-        System.out.println("austinSentence.length() = " + austinSentence.length());
-        System.out.println("txCapitalCity = " + txCapitalCity);
-        System.out.println("txCapitalCity.length() = " + txCapitalCity.length());
-        System.out.println();
-
-        //String .replace(searchPattern, replacementString) - Returns a copy of the string with the matching pattern replaced by the second argument
+//        //String .replace(searchPattern, replacementString) - Returns a copy of the string with the matching pattern replaced by the second argument
         String newcapitalSentence = austinSentence.replace("is Austin, and it is", "was five other cities before Austin, and the state is");
         System.out.println("newcapitalSentence = " + newcapitalSentence);
-
+//
         //.toLowerCase() & toUpperCase()
         System.out.println("txCapitalCity = " + txCapitalCity);
         System.out.println("txCapitalCity.toLowerCase() = " + txCapitalCity.toLowerCase());

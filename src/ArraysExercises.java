71a4f6df02e3d2abe466dc2ebe2071c195c8d244
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-01 10:20:44 -0500

    Arrays Exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,2 +1,24 @@
 public class ArraysExercises {
+
+
+
+	public static void main(String[] args){
+		double[] prices = new double[4];
+		final int numberOfSides = 6;
+		int[] diceSides = new int[];
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+	}
+
+
+
+
+
+
+
+
+
+
 }

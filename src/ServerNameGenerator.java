96711cf126939bcd69952c3abb01f3b84f39b467
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-01 15:26:05 -0500

    Arrays Exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,11 @@
 public class ServerNameGenerator {
+
+
+
+	public static void main(String[] args){
+		String[] adjectives = {"dorable","adventurous", "aggressive", "ashamed","attractive","average", "awful",	"bad",	"beautiful", "better"};
+		String[] noun = {"man", "woman", "teacher", "John", "Mary", "home", "town", "countryside", "America", "table"};
+
+
+	}
 }
---
+++ src/movies/Movie.java
@@ -0,0 +1,11 @@
+package movies;
+
+public class Movie {
+private String name;
+private String category;
+
+	public  setNMovieame(String name, String category){
+
+	}
+
+}

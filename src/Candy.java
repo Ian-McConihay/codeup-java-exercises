e9904b5ebdd277acaf8556c82e023c0b0d764b9c
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-01 10:16:20 -0500

    Arrays Exercise

--- src/Candy.java
+++ src/Candy.java
@@ -1,19 +1,24 @@
 public class Candy {
+//	Two public fields. Taking in string literal values set to name and size
 	private String name;
 	private String size;
-
-	public String getSize() {
-		return size;
-	}
-
-	public String getName() {
-		return name;
-	}
 
 	public void setNameAndSize(String name, String size) {
 		this.name = name;
 		this.size = size;
 	}
+
+
+	public String getSize() {
+		return size;
+	}
+	public String getName() {
+		return name;
+	}
+//	public Candy(String name, String Size){
+//
+//	}
+
 
 	public void sayAhhh() {
 		System.out.println("Ahhhh, a " + getName() + "!" + " I see that you got the " +getSize() + " of it. Sound good!");

7bf6baa36ccdfae9d7f3cd265bd5ff571cb7714d
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-02 10:24:16 -0500

    Morning warm up

--- src/Candy.java
+++ src/Candy.java
@@ -2,12 +2,19 @@ public class Candy {
 //	Two public fields. Taking in string literal values set to name and size
 	private String name;
 	private String size;
+	private String descriptor;
+	private String[] descriptorOne;
 
-	public void setNameAndSize(String name, String size) {
+	public void setNameAndSizeandDescriptor(String name, String size, String descriptor, String[] descriptorOne) {
+		this.descriptor = descriptor;
+		this.descriptorOne =descriptorOne;
 		this.name = name;
 		this.size = size;
 	}
 
+	public String[] getDescriptorOne() {
+		return descriptorOne;
+	}
 
 	public String getSize() {
 		return size;
@@ -15,19 +22,24 @@ public class Candy {
 	public String getName() {
 		return name;
 	}
+	public String getDescriptor(){
+		return descriptor;
+	}
 //	public Candy(String name, String Size){
 //
 //	}
 
 
 	public void sayAhhh() {
-		System.out.println("Ahhhh, a " + getName() + "!" + " I see that you got the " +getSize() + " of it. Sound good!");
+		System.out.println("I have a piece of " + getName() + " that is " +getSize() + " and could be described as " + getDescriptor());
 	}
 
 
 	public static void main(String[] args) {
+//		String myBag[] = {"Candy coated", "Chocolate", "Sour"};
+//		for(String bag : myBag)
 		Candy candyOne = new Candy();
-		candyOne.setNameAndSize("Kit-Kat", "Large");
+//		candyOne.setNameAndSizeandDescriptor("Kit-Kat", "Large", "chocolate");
 		candyOne.sayAhhh();
 	}
 }
